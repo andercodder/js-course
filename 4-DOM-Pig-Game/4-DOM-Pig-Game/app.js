203f8f6dcd297aebf67305b8e178897571754444
andercodder <andrevmc1000@hotmail.com>
2018-07-23 22:24:17 +0100

    update

--- 4-DOM-Pig-Game/4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/4-DOM-Pig-Game/app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer;
 
 scores =[0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 
 document.querySelector('.dice').style.display = 'none'; //setter -> when we set something
@@ -34,6 +34,26 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
    diceDOM.src = 'dice-' + dice + '.png';
 
    //3.update the round score IF the rolled number was NOT a 1
+   if(dice !== 1){
+       //Add score
+       roundScore += dice;
+       document.querySelector('#current-' + activePlayer).textContent = roundScore;
+   }else{
+       //Next Player
+       activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // if active player is 0 i want it to be 1 ao contrario eu quero que ele seje zero
+       roundScore = 0;
+       
+       //update the score
+       document.getElementById('current-0').textContent = '0';
+       document.getElementById('current-1').textContent = '0';
+
+       document.querySelector('.player-0-panel').classList.toggle('active');
+       document.querySelector('.player-1-panel').classList.toggle('active');
+
+       document.querySelector('.dice').style.display = 'none';
+
+
+   }
    
 });
 
