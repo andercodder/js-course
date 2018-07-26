7da731cd9bbf5d6f27db418fa5a9ad5aec47f6ec
andercodder <andrevmc1000@hotmail.com>
2018-07-26 21:04:29 +0100

    new game funcionality added

--- 4-DOM-Pig-Game/4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/4-DOM-Pig-Game/app.js
@@ -9,87 +9,113 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;  
+var scores, roundScore, activePlayer;
 
-scores =[0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
 
-document.querySelector('.dice').style.display = 'none'; //setter -> when we set something
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-document.querySelector('.btn-roll').addEventListener('click', function(){
+document.querySelector('.btn-roll').addEventListener('click', function () {
 
     //1.random number
-   var dice = Math.floor(Math.random() * 6) + 1;
+    var dice = Math.floor(Math.random() * 6) + 1;
 
-   //2.display the result
-   var diceDOM = document.querySelector('.dice');
-   diceDOM.style.display = 'block';
-   diceDOM.src = 'dice-' + dice + '.png';
+    //2.display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
-   //3.update the round score IF the rolled number was NOT a 1
-   if(dice !== 1){
-       //Add score
-       roundScore += dice;
-       document.querySelector('#current-' + activePlayer).textContent = roundScore;
-   }else{ 
-       //Next Player
-       nextPlayer();
+    //3.update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next Player
+        nextPlayer();
 
 
-   }
-   
+    }
+
 });
 
-document.querySelector('.btn-hold').addEventListener('click', function(){
+document.querySelector('.btn-hold').addEventListener('click', function () {
     //Add CURRENT score to GlOBAL score
     //console.log(scores[activePlayer]);
     scores[activePlayer] += roundScore;
     //console.log(scores[activePlayer]);
 
     //Update the UI
-    document.querySelector('#score-' +  activePlayer).textContent = scores[activePlayer];
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
     //check if player won the game
-    if(scores[activePlayer] >= 20){
+    if (scores[activePlayer] >= 20) {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 
-    }else{
+    } else {
         //Next player 
-    nextPlayer();
+        nextPlayer();
     }
-    
+
 })
 
 
-function nextPlayer(){
+function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // if active player is 0 i want it to be 1 ao contrario eu quero que ele seje zero
-       roundScore = 0;
-       
-       //update the score
-       document.getElementById('current-0').textContent = '0';
-       document.getElementById('current-1').textContent = '0';
+    roundScore = 0;
 
-       document.querySelector('.player-0-panel').classList.toggle('active');
-       document.querySelector('.player-1-panel').classList.toggle('active');
+    //update the score
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
 
-       document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
 }
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+
+    document.querySelector('.dice').style.display = 'none'; //setter -> when we set something
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1!';
+    document.getElementById('name-1').textContent = 'Player 2!';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+
+
+    document.querySelector('.player-0-panel').classList.add('active');
+
+
+
+
+
+}
+
 
 /* document.querySelector('#current-' + activePlayer).textContent = dice; //getter
 //document.querySelectoe('#current-' + activePlayer).innerHTML = '<em>' + dice +'</em>';
 
 var x = document.querySelector('#score-0').textContent;
 console.log(x); */
-
-
-
