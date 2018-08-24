0f5874c069f549a3d33fb1a6b1b3dfb3462181a9
andercodder <andrevmc1000@hotmail.com>
2018-08-24 16:34:28 +0100

    next

--- 5-advanced-JS/starter/challenge.js
+++ 5-advanced-JS/starter/challenge.js
@@ -1,25 +1,35 @@
- function Questions(question, answer, correct) {
-    this.question = question;
-    this.answer = answer;
-    this.correct = correct;
-   
-}
-Questions.prototype.displayQuestions = function(){
+(function(){
+    function Questions(question, answer, correct) {
+        this.question = question;
+        this.answer = answer;
+        this.correct = correct;
+
+    }
+Questions.prototype.displayQuestions = function () {
     console.log(this.question);
-    for (var i = 0; i< this.answer.length; i++){
-      console.log(i + ': ', this.answer[i]);
+    for (var i = 0; i < this.answer.length; i++) {
+        console.log(i + ': ' + this.answer[i]);
     }
 }
 
-var question1 = new Questions ("andre is the coolest person here any here?", ["yes", "no"], 0);
+Questions.prototype.checkAnswer = function (ans) {
+    if (ans === this.correct) {
+        console.log('correct answer!');
+    } else {
+        console.log('Wrong answer. Try again')
+    }
+}
+
+var question1 = new Questions("andre is the coolest person here any here?", ["yes", "no"], 0);
 var question2 = new Questions("is this a teaser ?", ["yes", "no"], 1);
 var question3 = new Questions("Is Js the Coolest language?", ["yes", "no"], 0);
 
- var questions = [question1, question2, question3];
- var choosequestion = Math.floor(Math.random() * questions.length);
- //console.log(questions[choosequestion]);
+var questions = [question1, question2, question3];
+var choosequestion = Math.floor(Math.random() * questions.length);
+//console.log(questions[choosequestion]);
 
- questions[choosequestion].displayQuestions();
-//var userA = prompt("Your Answer ?");
-//console.log(userA);
-//check(userA);
+questions[choosequestion].displayQuestions();
+var answer = parseInt(prompt('please select the answer'));
+
+questions[choosequestion].checkAnswer(answer);
+})(); //the fact that the code is inside of a fuction it makes it private this is IFFE
